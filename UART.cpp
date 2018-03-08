b81a5d57a4e461c5952ddd7fe399d5d54c8ce2f4
Tarcontar <michael-schnabel@gmx.net>
2018-03-08 18:54:16 +0100

    fixed receive and transmit

--- UART.cpp
+++ UART.cpp
@@ -4,172 +4,240 @@
 
 #define BUFFER_SIZE 128
 
-volatile data v_buffer[BUFFER_SIZE];
-volatile int v_pos = 0;
-volatile int v_end = 0;
+volatile uint16_t v_buffer[BUFFER_SIZE];
+volatile uint16_t v_start = 0;
+volatile uint16_t v_end = 0;
 volatile bool v_error = false;
-volatile bool v_msg_complete = false;
-volatile int v_uart = 0;
+volatile bool v_ninthBitSet = false;
+volatile uint8_t v_uart = 0;
 
-volatile unsigned char *m_UDRn;
-volatile unsigned char *m_UBRRnH;
-volatile unsigned char *m_UBRRnL;
-volatile unsigned char *m_UCSRnA;
-volatile unsigned char *m_UCSRnB;
-volatile unsigned char *m_UCSRnC;
+volatile uint8_t *v_UDRn;
+volatile uint8_t *v_UBRRnH;
+volatile uint8_t *v_UBRRnL;
+volatile uint8_t *v_UCSRnA;
+volatile uint8_t *v_UCSRnB;
+volatile uint8_t *v_UCSRnC;
 
-UART* UART::UART8Bit(int uart)
+uint8_t RXENn;
+uint8_t TXENn;
+uint8_t RXCIEn;
+uint8_t UCSZn0;
+uint8_t UCSZn1;
+uint8_t UCSZn2;
+uint8_t USBSn;
+uint8_t U2Xn;
+
+
+UART* UART::UART8Bit(uint8_t uart)
 {
 	return new UART(uart);
 }
 
-UART* UART::UART9Bit(int uart)
+UART* UART::UART9Bit(uint8_t uart)
 {
 	return new UART(uart, true);
 }
 
-UART::UART(int uart, bool nine_bit)
+UART::UART(uint8_t uart, bool nine_bit)
 {
-	v_uart = uart;
-	if (uart == 2)
+	v_uart = uart % 4;
+	if (uart == 0)
+	{
+		m_TXn = 1;
+		RXENn = RXEN0;
+		TXENn = TXEN0;
+		UCSZn0 = UCSZ00;
+		UCSZn1 = UCSZ01;
+		UCSZn2 = UCSZ02;
+		USBSn = USBS0;
+		U2Xn = U2X0;
+		RXCIEn = RXCIE0;
+		
+		v_UDRn = &UDR0;
+		v_UBRRnH = &UBRR0H;
+		v_UBRRnL = &UBRR0L;
+		v_UCSRnA = &UCSR0A;
+		v_UCSRnB = &UCSR0B;
+		v_UCSRnC = &UCSR0C;
+	}
+	else if (uart == 2)
 	{
 		m_TXn = 16;
-		m_UDRn = &UDR2;
-		m_RXENn = RXEN2;
-		m_TXENn = TXEN2;
-		m_UBRRnH = &UBRR2H;
-		m_UBRRnL = &UBRR2L;
-		m_UCSRnA = &UCSR2A;
-		m_UCSRnB = &UCSR2B;
-		m_UCSRnC = &UCSR2C;
-		m_UCSZn0 = UCSZ20;
-		m_UCSZn1 = UCSZ21;
-		m_UCSZn2 = UCSZ22;
-		m_U2Xn = U2X2;
+		RXENn = RXEN2;
+		TXENn = TXEN2;
+		UCSZn0 = UCSZ20;
+		UCSZn1 = UCSZ21;
+		UCSZn2 = UCSZ22;
+		USBSn = USBS2;
+		U2Xn = U2X2;
+		RXCIEn = RXCIE2;
+		
+		v_UDRn = &UDR2;
+		v_UBRRnH = &UBRR2H;
+		v_UBRRnL = &UBRR2L;
+		v_UCSRnA = &UCSR2A;
+		v_UCSRnB = &UCSR2B;
+		v_UCSRnC = &UCSR2C;
 	}
 	else if (uart == 3)
 	{
 		m_TXn = 14;
-		m_UDRn = &UDR3;
-		m_RXENn = RXEN3;
-		m_TXENn = TXEN3;
-		m_UBRRnH = &UBRR3H;
-		m_UBRRnL = &UBRR3L;
-		m_UCSRnA = &UCSR3A;
-		m_UCSRnB = &UCSR3B;
-		m_UCSRnC = &UCSR3C;
-		m_UCSZn0 = UCSZ30;
-		m_UCSZn1 = UCSZ31;
-		m_UCSZn2 = UCSZ32;
-		m_U2Xn = U2X3;
+		RXENn = RXEN3;
+		TXENn = TXEN3;
+		UCSZn0 = UCSZ30;
+		UCSZn1 = UCSZ31;
+		UCSZn2 = UCSZ32;
+		USBSn = USBS3;
+		U2Xn = U2X3;
+		RXCIEn = RXCIE3;
+		
+		v_UDRn = &UDR3;
+		v_UBRRnH = &UBRR3H;
+		v_UBRRnL = &UBRR3L;
+		v_UCSRnA = &UCSR3A;
+		v_UCSRnB = &UCSR3B;
+		v_UCSRnC = &UCSR3C;
 	}
 	else
 	{
 		m_TXn = 18;
-		m_UDRn = &UDR1;
-		m_RXENn = RXEN1;
-		m_TXENn = TXEN1;
-		m_UBRRnH = &UBRR1H;
-		m_UBRRnL = &UBRR1L;
-		m_UCSRnA = &UCSR1A;
-		m_UCSRnB = &UCSR1B;
-		m_UCSRnC = &UCSR1C;
-		m_UCSZn0 = UCSZ10;
-		m_UCSZn1 = UCSZ11;
-		m_UCSZn2 = UCSZ12;
-		m_U2Xn = U2X1;
+		RXENn = RXEN1;
+		TXENn = TXEN1;
+		UCSZn0 = UCSZ10;
+		UCSZn1 = UCSZ11;
+		UCSZn2 = UCSZ12;
+		USBSn = USBS1;
+		U2Xn = U2X1;
+		RXCIEn = RXCIE1;
+		
+		v_UDRn = &UDR1;
+		v_UBRRnH = &UBRR1H;
+		v_UBRRnL = &UBRR1L;
+		v_UCSRnA = &UCSR1A;
+		v_UCSRnB = &UCSR1B;
+		v_UCSRnC = &UCSR1C;
 	}
-	
-	*m_UCSRnB = (1 << m_RXENn) | (1 << m_TXENn) | (1 << RXCIE1);
-	*m_UCSRnC |= (1 << m_UCSZn1) | (1 << m_UCSZn0); //8 bit mode
+	//set also UDRIE0 ?? 
+	*v_UCSRnB |= (1 << RXENn) | (1 << TXENn) | (1 << RXCIEn);
+	*v_UCSRnC |= (1 << UCSZn1) | (1 << UCSZn0); //8 bit mode
 	if (nine_bit)
-		*m_UCSRnB |= (1 << m_UCSZn2); //9 bit mode
-	*m_UCSRnC |= (0 << USBS1); //one stop bit else 1
-	//*m_UCSRnC |= 0b00000000; //no parity bit
+		*v_UCSRnB |= (1 << UCSZn2); 
+	*v_UCSRnC |= (0 << USBSn); //one stop bit else 1
+	*v_UCSRnC |= 0b00000000; //no parity bit
+}
+
+UART::~UART()
+{
+	end();
 }
 
 void UART::begin(int baud)
 {
-	uint16_t baud_setting = (F_CPU / 4 / baud - 1) / 2;
-	*m_UBRRnH = baud_setting >> 8;
-	*m_UBRRnL = baud_setting;
-	*m_UCSRnA &= ~(1 << m_U2Xn); //disable rate doubler
+	uint16_t baud_setting = (F_CPU / 8 / baud - 1) / 2;
+	*v_UBRRnH = baud_setting >> 8;
+	*v_UBRRnL = baud_setting;
+	*v_UCSRnA &= ~(1 << U2Xn); //disable rate doubler
 }
 
-bool UART::available()
+void UART::end()
 {
-	return ((unsigned int)(BUFFER_SIZE + v_pos - v_end)) % BUFFER_SIZE;
+	flush();
+	*v_UCSRnB |= (0 << RXENn) | (0 << TXENn) | (0 << RXCIEn);
 }
 
-int UART::print(String str)
+int UART::available()
 {
-	int i;
-	for (i = 0; i < str.length(); i++)
-		write((uint8_t)str[i]);
-	return i;
+	return ((int)(BUFFER_SIZE + v_end - v_start)) % BUFFER_SIZE;
 }
 
-void UART::write(uint8_t data, int mode)
+size_t UART::write(uint8_t data)
 {
-	while (!(*m_UCSRnA & (1 << UDRE))) {}
-	if (mode)
-		*m_UCSRnB |= (1 << TXB8);
+	v_ninthBitSet = false;
+	while (!(*v_UCSRnA & (1 << UDRE))) {}
+	*v_UDRn = data;
+}
+
+size_t UART::write9bit(uint16_t data)
+{
+	while (!(*v_UCSRnA & (1 << UDRE))) {}
+	if (data & 0x100)
+		*v_UCSRnB |= (1 << TXB8);
 	else
-		*m_UCSRnB &= ~(1 << TXB8);
-	*m_UDRn = data;
+		*v_UCSRnB &= ~(1 << TXB8);
+	write((uint8_t)data);
 }
 
 int UART::read()
 {
-	if (v_pos == v_end) {
+	if (v_start == v_end) {
 		return -1;
 	} else {
-		unsigned char c = v_buffer[v_pos].value;
-		v_pos = (v_pos + 1) % BUFFER_SIZE;
+		unsigned char c = v_buffer[v_start];
+		v_start = (v_start + 1) % BUFFER_SIZE;
 		return c;
 	}
 }
 
-//uart 0 causes compiler errors
-/*
+int UART::peek()
+{
+	if (v_start == v_end) {
+		return -1;
+	} else {
+		v_buffer[v_start];
+	}
+}
+
+void UART::flush()
+{
+	v_start = 0;
+	v_end = 0;
+}
+
+bool UART::error()
+{
+	return v_error;
+}
+
+bool UART::ninthBitSet()
+{
+	return v_ninthBitSet;
+}
+
 ISR(USART0_RX_vect)
 {
 	if (v_uart == 0)
 	{
-		char status = *m_UCSRnA;
-
+		char status = *v_UCSRnA;
 		if (status & ((1 << FE) | (1 << DOR) | (1 << UPE)))
 		{
 			v_error = true;
 		}
-		v_buffer[v_end].mode = (*m_UCSRnB >> 1) & 0x01;
-		if (v_buffer[v_end].mode == 1)
+		uint16_t result = ((*v_UCSRnB >> 1) & 0x01) << 8;
+		if (result & 0x100)
+			v_ninthBitSet = true;
+		result |= *v_UDRn;
+		v_buffer[v_end] = result;
+		v_end = (v_end + 1) % BUFFER_SIZE;
+		if (v_end == v_start)
 		{
-			//we got an ack or an checksum
-			v_msg_complete = true;
+			v_start = (v_start + 1) % BUFFER_SIZE;
 		}
-		v_buffer[v_end++].value = *m_UDRn;
 	}
 }
-*/
 
 ISR(USART1_RX_vect)
 {
 	if (v_uart == 1)
 	{
-		char status = *m_UCSRnA;
-
+		/*
+		char status = *v_UCSRnA;
 		if (status & ((1 << FE) | (1 << DOR) | (1 << UPE)))
 		{
 			v_error = true;
 		}
-		v_buffer[v_end].mode = (*m_UCSRnB >> 1) & 0x01;
-		if (v_buffer[v_end].mode == 1)
-		{
-			//we got an ack or an checksum
-			v_msg_complete = true;
-		}
-		v_buffer[v_end++].value = *m_UDRn;
+		v_buffer[v_end] = (*v_UCSRnB >> 1) & 0x01;
+		v_buffer[v_end++] = *v_UDRn;
+		*/
 	}
 }
 
@@ -177,19 +245,15 @@ ISR(USART2_RX_vect)
 {
 	if (v_uart == 2)
 	{
-		char status = *m_UCSRnA;
-
+		/*
+		char status = *v_UCSRnA;
 		if (status & ((1 << FE) | (1 << DOR) | (1 << UPE)))
 		{
 			v_error = true;
 		}
-		v_buffer[v_end].mode = (*m_UCSRnB >> 1) & 0x01;
-		if (v_buffer[v_end].mode == 1)
-		{
-			//we got an ack or an checksum
-			v_msg_complete = true;
-		}
-		v_buffer[v_end++].value = *m_UDRn;
+		v_buffer[v_end] = (*v_UCSRnB >> 1) & 0x01;
+		v_buffer[v_end++] = *v_UDRn;
+		*/
 	}
 }
 
@@ -197,18 +261,14 @@ ISR(USART3_RX_vect)
 {
 	if (v_uart == 3)
 	{
-		char status = *m_UCSRnA;
-
+		/*
+		char status = *v_UCSRnA;
 		if (status & ((1 << FE) | (1 << DOR) | (1 << UPE)))
 		{
 			v_error = true;
 		}
-		v_buffer[v_end].mode = (*m_UCSRnB >> 1) & 0x01;
-		if (v_buffer[v_end].mode == 1)
-		{
-			//we got an ack or an checksum
-			v_msg_complete = true;
-		}
-		v_buffer[v_end++].value = *m_UDRn;
+		v_buffer[v_end] = (*v_UCSRnB >> 1) & 0x01;
+		v_buffer[v_end++] = *v_UDRn;
+		*/
 	}
 }
--- UART.hpp
+++ UART.hpp
@@ -1,41 +1,44 @@
 #pragma once
 #include <Arduino.h>
+#include "Stream.h"
 
 //registers
 #define TXB8	0
-#define UPE	2
-#define DOR	3
+#define UPE		2
+#define DOR		3
 #define FE		4
 #define UDRE	5
-#define RXC	7
+#define RXC		7
 
-struct data
-{
-	char value;
-	char mode;
-};
+#define UART_BUFFER_SIZE 128
 
-class UART
+class UART : public Stream
 {
 public:
-	static UART* UART8Bit(int uart);
-	static UART* UART9Bit(int uart);
+	static UART* UART8Bit(uint8_t uart);
+	static UART* UART9Bit(uint8_t uart);
 	
-	void begin(int baud);
-	bool available();
-	int print(String str);
-	void write(uint8_t data, int mode = 0);
+	~UART();
+	
+	void begin(int baud = 9600);
+	void end();
+	int available();
+	int peek();
+	size_t write(uint8_t data);
+	inline size_t write(int data) { return write((uint8_t)data); }
+	size_t write9bit(uint16_t data);
+	
 	int read();
+	void flush();
+
+	bool error();
+	bool ninthBitSet();
+	inline uint8_t getTXPin() { return m_TXn; }
+	
+	using Print::write; // pull in write(str) and write(buf, size) from Print
 	
 private:
-	UART(int uart = 1, bool nine_bit = false);
+	UART(uint8_t uart = 1, bool nine_bit = false);
 
-	int m_TXn;
-	
-	volatile unsigned char  m_RXENn;
-	volatile unsigned char  m_TXENn;
-	volatile unsigned char  m_U2Xn;
-	volatile unsigned char  m_UCSZn0;
-	volatile unsigned char  m_UCSZn1;
-	volatile unsigned char  m_UCSZn2;
+	uint8_t m_TXn;
 };
